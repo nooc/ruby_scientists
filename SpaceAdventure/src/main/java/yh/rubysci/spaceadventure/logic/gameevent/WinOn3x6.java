743f1b8b85385087f6146d7212ce3a8e094f19a5
Ben Bright <nooc@users.noreply.github.com>
2022-12-05 10:48:02 +0100

    typo

--- SpaceAdventure/src/main/java/yh/rubysci/spaceadventure/logic/gameevent/WinOn3x6.java
+++ SpaceAdventure/src/main/java/yh/rubysci/spaceadventure/logic/gameevent/WinOn3x6.java
@@ -4,6 +4,6 @@ public class WinOn3x6 extends GameFinished {
     @Override
     public String getEventMessage(int roll) {
 
-        return "Yoy rolled a 6 three times in a row!\n" + super.getEventMessage(roll);
+        return "You rolled a 6 three times in a row!\n" + super.getEventMessage(roll);
     }
 }
